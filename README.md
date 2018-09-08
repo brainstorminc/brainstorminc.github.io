7760dd3b932fb7081f6749c4a702a157eafbf83b
Ronan <rsf.sho@gmail.com>
2018-09-08 16:40:08 -0700

    Cleaned for real development

--- README.md
+++
@@ -1,1 +0,0 @@
-# brainstorminc.github.io
--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('../node_modules/bulma/css/bulma.css')
-// require('../node_modules/aos/dist/aos.css')
-require('./styles.scss')
-require('./jquery.scrollTo.min.js')
-// require('../node_modules/aos/dist/aos.js')
-// import AOS from './aos.js '
-require('./main.js')
